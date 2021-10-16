14eb72ae01d7c7a8b96044b7f9801f33307b4660
Adrian W <windisch.adrian09@gmail.com>
2021-10-16 20:59:25 +0300

    'fix my stupidity'

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ require('dotenv').config()
 
 const { Client, Intents, Channel } = require('discord.js');
 const { getVoiceConnection, joinVoiceChannel } = require('@discordjs/voice');
-const client = new Client({ intents: [Intents.FLAGS.GUILDS, Intents.FLAGS.GUILD_MESSAGES, Intents.FLAGS.GUILD_VOICE_STATES] });
+const client = new Client({ intents: 641 });
 const replyTimeout = 100000;
 isReady = false;
 
