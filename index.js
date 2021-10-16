69ec82a6892adcc22f0dae63e00e88b0dd38271d
Adrian W <windisch.adrian09@gmail.com>
2021-10-16 21:48:02 +0300

    'fix my stupidity'

--- index.js
+++ index.js
@@ -44,11 +44,8 @@ function voiceChannelJoin(message, voiceChannel) {
 }
 
 function voiceChannelLeave(message, voiceChannel) {
-    console.log('channel: ' + message.guild.me.voice.channel);
-    console.log('voice: ' + message.guild.me.voice);
-    if (!message.guild.me.voice.channel) return message.reply("Not in any voice channel.");
-
-    const connection = getVoiceConnection(voiceChannel.guildId);
+    let connection = message.guild.voiceConnection;
+    if (!connection) return message.reply("Not in any voice channel.");
     connection.disconnect();
     connection.destroy();
 }
