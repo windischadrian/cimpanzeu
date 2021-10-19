e713db2ab1bcdc51f13a3a6a0966a09ab83abeb0
Adrian W <windisch.adrian09@gmail.com>
2021-10-19 11:09:50 +0300

    'add allowed users'

--- index.js
+++ index.js
@@ -10,6 +10,8 @@ const {
     NoSubscriberBehavior
 } = require('@discordjs/voice');
 const client = new Client({ intents: ["GUILDS", "GUILD_MESSAGES", "GUILD_VOICE_STATES"] });
+const CronJob = require('cron').CronJob;
+const bahoiSuicid = 'Oricâte probleme ai avea, nu-i bine să te omori ca țăranu...te omori ieee ca pizdele alea \'mă omor miaaa\', dar tu nu mai exiști, adică tu crezi că ajungi in altă lume și pula mea cu spiritul tău..NU! Spiritul rămane...in pizda mă-tii1 Știți ce e important? Să futi, să te distrezi, să bei, mici, bere, ăăăă și toate cele, pepeni, struguri, ciorchini, toate distracțiile toate panaramele sunt atâtea carusele sunt atâtea mașinării care să apeși pe butoane sunt atâtea magarii sa canti sa vioara sa toate alea ce dracu ma te omori ce pizda mă-...psaico maico ca esti bolnav la cap că nu te placi ca pula mea ca pula-n pizdă da mai lasă-mă-n pula mea crezi ca mă omor acuma ca dacă nu mai nu stiu ce mai lasă nu știu ce...\'mă omor\' vai băga-mi-aș pula-n mă-ta te omori? Mai lasă-mă in pula mea cu omorâtu pizdii.';
 const playdl = require('play-dl');
 // const ytsr = require('ytsr');
 const ytsr = require('youtube-search-without-api-key');
@@ -45,6 +47,8 @@ client.login(process.env.BOT_TOKEN);
 client.on("ready", () => {
     isReady = true;
     console.log("Bot is ready");
+    cafelutsaCronJob(client).start();
+    change();
 })
 
 
@@ -54,7 +58,7 @@ client.on("message", async message => {
     if (message.author.bot) return;
 
     let voiceChannel = message.member.voice.channel;
-    let messageText = message.content.toLowerCase();
+    let messageText = message.content;
     
     if(messageText.startsWith(`${prefix}join`)) voiceChannelJoin(message, voiceChannel);
 
@@ -68,8 +72,11 @@ client.on("message", async message => {
 
     if(messageText.startsWith(`${prefix}q`)) executeQueueueueCommand(message);
 
-})
+    messageText = messageText.toLowerCase();
+    
+    hei(message, messageText);
 
+})
 
 function voiceChannelJoin(message, voiceChannel) {
     if (!voiceChannel) return message.reply("You need to be in a voice channel.");
@@ -159,10 +166,8 @@ async function executePlayCommand(message, voiceChannel) {
 }
 
 async function searchYoutubeAsync(songName) {
-    console.log('SongName: ' + songName);
     const videoResult = await ytsr.search(songName);
     const songInfo = videoResult[0];
-    console.log('Song info: ' + songInfo);
     return songInfo;
 }
 
@@ -181,7 +186,6 @@ async function play(message) {
     }
 
     try {
-        console.log('song url: ' + song.url)
         const stream = await playdl.stream(song.url);
         let resource = createAudioResource(stream.stream, {
             inputType: stream.type
@@ -249,3 +253,40 @@ function executeQueueueueCommand(message) {
     }
     
 }
+
+function hei(message, messageText) {
+    if (messageText.includes('miau')) {
+        message.reply('Imi bag pula in mieunatu tau coaie');
+    } else if (messageText.includes('babilon')) {
+        message.reply('Baga-ti-as babilonu in cur');
+    } else if (messageText.match(/(ei|3|ei\?|ei \?|3\?|3 \?)$/)) {
+        message.reply('De pula sa ma iei hei hei');
+    } else if (messageText.endsWith('5') || messageText.endsWith('cinci')) {
+        message.reply('In cur ma lingi');
+    } else if (messageText.match(/(arici|arici\?|arici \?)$/)) {
+        message.reply('Du-te-n pula mea de aici');
+    } else if (messageText.match(/(sinucid|suicid|ma omor)/)) {
+        message.reply(bahoiSuicid);
+    }
+}
+
+const altName = ['Sandei', 'Mondei', 'Tvesdei', 'Vednesdei', 'Sărzdei', 'Freidei', 'Seturdei', ];
+const date = new Date();
+
+function change() {
+
+    var welfareVoiceChannel =  client.channels.cache.get('698958944122699878');
+    var day = date.getDay();
+    var newName = 'Welfare ' + altName[day];
+
+    welfareVoiceChannel.setName(newName);
+}
+
+setInterval(change, 1000000);
+
+const cafelutsaCronJob = (client) => new CronJob('00 00 09 * * *', () => {
+
+    var generalTextChannel = client.channels.cache.get('788439966975000576');
+    generalTextChannel.send('Va urez spor la cafelutsa si sa aveti o zi minunata!');
+
+}, null, true, 'Europe/Bucharest');
